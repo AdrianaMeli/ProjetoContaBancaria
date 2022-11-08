b556d4782bca8b3de6b4d5c75de7c095dbd82a75
AdrianaMeli <adriana.nogueira@mercadolivre.com>
2022-11-08 12:01:11 -0300

    Trabalhando conta bancaria aula Rafael

--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -1,13 +1,14 @@
-package model;
+package conta.model;
 
-public class model {
+public abstract class Conta {
+
     private int numero;
     private int agencia;
     private int tipo;
     private String titular;
     private float saldo;
 
-    public model(int numero, int agencia, int tipo, String titular, float saldo) {
+    public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
         this.numero = numero;
         this.agencia = agencia;
         this.tipo = tipo;
@@ -57,41 +58,43 @@ public class model {
 
     public boolean sacar(float valor) {
 
-        if (getSaldo() < valor) {
+        if(this.getSaldo() < valor) {
             System.out.println("\n Saldo Insuficiente!");
             return false;
         }
 
-        this.setSaldo(getSaldo() - valor);
+        this.setSaldo(this.getSaldo() - valor);
         return true;
+    }
+
+    public void depositar(float valor) {
+
+        this.setSaldo(this.getSaldo() + valor);
+
     }
 
     public void visualizar() {
 
         String tipo = "";
 
-        switch (this.tipo) {
+        switch(this.tipo) {
             case 1:
                 tipo = "Conta Corrente";
                 break;
             case 2:
                 tipo = "Conta Poupança";
                 break;
-
         }
 
-                System.out.println("\n\n*********************************************************************");
-                System.out.println("Dados da Conta:");
-                System.out.println("*********************************************************************");
-                System.out.println("Numero da Conta: " + this.numero);
-                System.out.println("Agência: " + this.agencia);
-                System.out.println("Tipo da Conta: " + tipo);
-                System.out.println("Titular: " + this.titular);
-                System.out.println("Saldo: " + this.saldo);
+        System.out.println("\n\n*********************************************************************");
+        System.out.println("Dados da Conta:");
+        System.out.println("*********************************************************************");
+        System.out.println("Numero da Conta: " + this.numero);
+        System.out.println("Agência: " + this.agencia);
+        System.out.println("Tipo da Conta: " + tipo);
+        System.out.println("Titular: " + this.titular);
+        System.out.println("Saldo: " + this.saldo);
 
-            }
+    }
 
-
-        }
-
-
+}
